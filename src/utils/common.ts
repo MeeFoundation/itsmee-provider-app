fe9b51414374f86cb2e73067adec07c73c1bd397
Raman Siliuk <rsiluk@swiftinvention.com>
2025-03-04 10:56:46 +0400

    mee-components updated, added sign-up-soon and license pages, changed filter on apps page

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,6 +1,19 @@
 import { Cookie } from "mee-components";
 
-export function checkProvider(authPages?: boolean) {
+export function checkProvider(authPages?: boolean, signUpSoonPage?: boolean) {
+  const signUpAvailable = Cookie.getCookie("sign-up-available");
+  if (!signUpAvailable && !signUpSoonPage) {
+    window.location.hash = "sign-up-soon";
+    return false;
+  } else if (signUpAvailable && signUpSoonPage) {
+    window.location.hash = "sign-in";
+    return false;
+  }
+
+  if (signUpSoonPage) {
+    return false;
+  }
+
   const provider = Cookie.getCookie("provider");
   if (!authPages && !provider) {
     window.location.hash = "sign-in";
@@ -12,7 +25,7 @@ export function checkProvider(authPages?: boolean) {
 
   if (!authPages && provider && window.location.hash) {
     const agreementConfirmed = Cookie.getCookie("agreement-confirmed");
-    if (!agreementConfirmed) {
+    if (!agreementConfirmed && window.location.hash != "#declined-license") {
       window.location.hash = "declined-license";
       return false;
     }
@@ -20,3 +33,4 @@ export function checkProvider(authPages?: boolean) {
 
   return true;
 }
+
